5a997041afaf1a8ccdadb089e1dcf14572ccf1fd
philyeahz <philyeahz@googlemail.com>
2020-07-14 00:46:08 +0200

    Experimental blender branch

--- import_mot.py
+++ import_mot.py
@@ -4,7 +4,19 @@ import bpy
 from bpy_extras.wm_utils.progress_report import ProgressReport
 
 from .modules.ShenmueDKPy.files.animation.motn import MOTN
+from .modules.ShenmueDKPy.utils.model import *
 
+ShenmueRigMap = {
+    'IKBoneID.Root': 'BoneID.Root',
+    #'IKBoneID.Spine': 'BoneID.Spine',
+    #'IKBoneID.Hip': 'BoneID.Hip',
+    #'IKBoneID.HandIKTarget_R': 'BoneID.Hand_IK_R',
+    #'IKBoneID.HandIKTarget_L': 'BoneID.Hand_IK_L',
+    'IKBoneID.FootIKTarget_R': 'BoneID.Foot_IK_R',
+    'IKBoneID.FootIKTarget_L': 'BoneID.Foot_IK_L',
+    #'IKBoneID.Shoulder_R': 'BoneID.Shoulder_R',
+    #'IKBoneID.Shoulder_L': 'BoneID.Shoulder_L',
+}
 
 def load(context, filepath):
     """
@@ -22,23 +34,55 @@ def load(context, filepath):
         motn.read(filepath)
         progress.leave_substeps("Done.")
 
-        print(bpy.context.selected_bones)
-        print(bpy.context.selected_objects[0])
+        # get the armature
+        armature = bpy.context.scene.objects['ShenmueRig']
+        bpy.context.view_layer.objects.active = armature
+        armature.select_set(True)
+        bpy.ops.object.mode_set(mode='POSE')
+
+        # set rotation mode to euler angle for all bones
+        for bone in armature.pose.bones:
+            bone.rotation_mode = "XYZ"
+
         progress.enter_substeps(2, "Loading sequences to selected rig...")
         for sequence in motn.sequences:
-            print(sequence.data.bone_keyframes)
-            None
-            # create a new cube
-            #bpy.ops.mesh.primitive_cube_add()
 
-            # newly created cube will be automatically selected
-            #cube = bpy.context.selected_objects[0]
+            # debug walk selection
+            if sequence.name == "A_WALK_L_02":
+                print("A_WALK_L_02\n")
+                for bone_data in sequence.data.bone_keyframes:
+                    bone_id = str(IKBoneID(bone_data.bone_index))
+                    print(bone_id)
+                    if bone_id in ShenmueRigMap:
+                        bone_name = ShenmueRigMap[bone_id]
+                        for bone in armature.pose.bones:
 
-            # change name
-            #cube.name = sequence.name
+                            if bone.name == bone_name:
 
-            # change its location
-            #cube.location = (0.0, 0.0, 0.0)
+                                print(bone_id, bone_name)
+
+                                for pos_x in bone_data.pos_x:
+                                    bone.location = [pos_x.value, 0.0, 0.0]
+                                    bone.keyframe_insert(data_path="location", index=0, frame=pos_x.frame)
+                                for pos_y in bone_data.pos_z:
+                                    bone.location = [0.0, pos_y.value, 0.0]
+                                    bone.keyframe_insert(data_path="location", index=1, frame=pos_y.frame)
+                                for pos_z in bone_data.pos_y:
+                                    bone.location = [0.0, 0.0, pos_z.value]
+                                    bone.keyframe_insert(data_path="location", index=2, frame=pos_z.frame)
+
+                                for rot_x in bone_data.rot_x:
+                                    bone.rotation_euler = [rot_x.value, 0.0, 0.0]
+                                    bone.keyframe_insert(data_path="rotation_euler", index=0, frame=rot_x.frame)
+                                for rot_y in bone_data.rot_z:
+                                    bone.rotation_euler = [0.0, rot_y.value, 0.0]
+                                    bone.keyframe_insert(data_path="rotation_euler", index=1, frame=rot_y.frame)
+                                for rot_z in bone_data.rot_y:
+                                    bone.rotation_euler = [0.0, 0.0, rot_z.value]
+                                    bone.keyframe_insert(data_path="rotation_euler", index=2, frame=rot_z.frame)
+
+                                break
+
         progress.leave_substeps("Done.")
 
         progress.leave_substeps("Finished importing: %r" % filepath)
